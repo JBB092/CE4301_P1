7fed17a8a393ce32191f7cc2862263a3d5ec3148
Jose Barquero Bonilla <jos.barquero@estudiantec.cr>
2025-08-25 19:24:45 -0600

    [UPDATE] Cambio a variable para el número de vueltas

--- Codigo_Fuente/proyecto-tea/src/tea.h
+++ Codigo_Fuente/proyecto-tea/src/tea.h
@@ -4,6 +4,7 @@
 #include <stdint.h>
 
 #define DELTA 0x9e3779b9
+#define NUM_ROUNDS 32
 
 void tea_encrypt(uint32_t v[2], const uint32_t key[4]);
 void tea_decrypt(uint32_t v[2], const uint32_t key[4]);
