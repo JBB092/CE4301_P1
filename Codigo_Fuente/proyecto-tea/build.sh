433277e5450065f67bd72c706fff6c9030ef12e4
Jose Barquero Bonilla <jos.barquero@estudiantec.cr>
2025-08-26 22:24:40 -0600

    [ADD][UPDATE] Creación de archivo startup.s para inicialización del .c y actualización de archivos .sh y linker.ld

--- Codigo_Fuente/proyecto-tea/build.sh
+++ Codigo_Fuente/proyecto-tea/build.sh
@@ -3,14 +3,27 @@
 
 echo "Compilando proyecto TEA..."
 
+# Compilar startup.s
+riscv64-unknown-elf-gcc \
+    -march=rv32im \
+    -mabi=ilp32 \
+    -nostdlib \
+    -ffreestanding \
+    -g3 -gdwarf-4 \
+    -c src/startup.s -o startup.o
+
+if [ $? -ne 0 ]; then
+    echo "Error en la compilación de startup.s"
+    exit 1
+fi
+
 # Compilar main.c
 riscv64-unknown-elf-gcc \
     -march=rv32im \
     -mabi=ilp32 \
     -nostdlib \
     -ffreestanding \
-    -g3 \
-    -gdwarf-4 \
+    -g3 -gdwarf-4 \
     -c src/main.c -o main.o
 
 if [ $? -ne 0 ]; then
@@ -24,8 +37,7 @@ riscv64-unknown-elf-gcc \
     -mabi=ilp32 \
     -nostdlib \
     -ffreestanding \
-    -g3 \
-    -gdwarf-4 \
+    -g3 -gdwarf-4 \
     -c src/tea.c -o tea.o
 
 if [ $? -ne 0 ]; then
@@ -39,15 +51,14 @@ riscv64-unknown-elf-gcc \
     -mabi=ilp32 \
     -nostdlib \
     -ffreestanding \
-    -g3 \
-    -gdwarf-4 \
-    main.o tea.o \
+    -g3 -gdwarf-4 \
+    startup.o main.o tea.o \
     -T linker.ld \
     -o test.elf
 
 if [ $? -eq 0 ]; then
-    echo "Compilación y enlace exitoso test.elf generado"
-    echo "Archivos objeto main.o, tea.o"
+    echo "Compilación y enlace exitoso: test.elf generado"
+    echo "Archivos objeto: startup.o, main.o, tea.o"
 else
     echo "Error en el enlace"
     exit 1
