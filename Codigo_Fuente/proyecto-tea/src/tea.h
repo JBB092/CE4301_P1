1f5b7943ca57d30ce82601af3913874653861b5d
Jose Barquero Bonilla <jos.barquero@estudiantec.cr>
2025-08-28 07:18:53 -0600

    [UPDATE] Actualización del header TEA en C

--- Codigo_Fuente/proyecto-tea/src/tea.h
+++ Codigo_Fuente/proyecto-tea/src/tea.h
@@ -3,10 +3,28 @@
 
 #include <stdint.h>
 
-#define DELTA 0x9e3779b9
+#define DELTA 0x9e3779b9u
 #define NUM_ROUNDS 32
 
+/* Prototipos en C (implementación de referencia) */
 void tea_encrypt(uint32_t v[2], const uint32_t key[4]);
 void tea_decrypt(uint32_t v[2], const uint32_t key[4]);
 
+/* Prototipos en ASM (implementaciones RISC-V) */
+void tea_encrypt_asm(uint32_t v[2], const uint32_t key[4]);
+void tea_decrypt_asm(uint32_t v[2], const uint32_t key[4]);
+
+/*
+ * Selector de implementación:
+ * - Si compilas con -DTEA_USE_ASM, redirige tea_encrypt/decrypt
+ *   a las versiones en ensamblador.
+ * - Si NO defines TEA_USE_ASM, usas la versión C (útil para comparar).
+ */
+#ifdef TEA_USE_ASM
+#  undef  tea_encrypt
+#  undef  tea_decrypt
+#  define tea_encrypt  tea_encrypt_asm
+#  define tea_decrypt  tea_decrypt_asm
 #endif
+
+#endif /* TEA_H */
