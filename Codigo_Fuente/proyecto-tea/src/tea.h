12ef712fafd18fbaf64c2dd07b4968c45ecff1b3
Jose Barquero Bonilla <jos.barquero@estudiantec.cr>
2025-08-28 07:24:58 -0600

    [UPDATE] Actualización en main.c para llamar las funciones ASM

--- Codigo_Fuente/proyecto-tea/src/tea.h
+++ Codigo_Fuente/proyecto-tea/src/tea.h
@@ -16,9 +16,9 @@ void tea_decrypt_asm(uint32_t v[2], const uint32_t key[4]);
 
 /*
  * Selector de implementación:
- * - Si compilas con -DTEA_USE_ASM, redirige tea_encrypt/decrypt
+ * - Si se compila con -DTEA_USE_ASM, redirige tea_encrypt/decrypt
  *   a las versiones en ensamblador.
- * - Si NO defines TEA_USE_ASM, usas la versión C (útil para comparar).
+ * - Si NO se define TEA_USE_ASM, se usa la versión C (útil para comparar).
  */
 #ifdef TEA_USE_ASM
 #  undef  tea_encrypt
