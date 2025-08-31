f0db49e104ce1b0fec1ee9f2146ea9d6efed99db
Jose Barquero Bonilla <jos.barquero@estudiantec.cr>
2025-08-31 14:43:17 -0600

    [ADD] Ambiente de trabajo con QEMU, Docker, GDB, etc

---
+++ Codigo_Fuente/baremetal-printf/build.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+# Build script para baremetal-printf
+
+echo "Limpiando archivos objeto y ejecutable previos..."
+rm -f startup.o main.o syscalls.o uart.o test.elf
+
+echo "Compilando proyecto baremetal-printf..."
+
+# Compilar startup.s
+riscv64-unknown-elf-gcc \
+    -march=rv32im \
+    -mabi=ilp32 \
+    -nostdlib \
+    -ffreestanding \
+    -g3 -gdwarf-4 \
+    -c src/startup.s -o startup.o
+if [ $? -ne 0 ]; then
+    echo "Error en la compilación de startup.s"
+    exit 1
+fi
+
+# Compilar main.c
+riscv64-unknown-elf-gcc \
+    -march=rv32im \
+    -mabi=ilp32 \
+    -nostdlib \
+    -ffreestanding \
+    -g3 -gdwarf-4 \
+    -c src/main.c -o main.o
+if [ $? -ne 0 ]; then
+    echo "Error en la compilación de main.c"
+    exit 1
+fi
+
+# Compilar syscalls.c
+riscv64-unknown-elf-gcc \
+    -march=rv32im \
+    -mabi=ilp32 \
+    -nostdlib \
+    -ffreestanding \
+    -g3 -gdwarf-4 \
+    -c src/syscalls.c -o syscalls.o
+if [ $? -ne 0 ]; then
+    echo "Error en la compilación de syscalls.c"
+    exit 1
+fi
+
+# Compilar uart.c
+riscv64-unknown-elf-gcc \
+    -march=rv32im \
+    -mabi=ilp32 \
+    -nostdlib \
+    -ffreestanding \
+    -g3 -gdwarf-4 \
+    -c src/uart.c -o uart.o
+if [ $? -ne 0 ]; then
+    echo "Error en la compilación de uart.c"
+    exit 1
+fi
+
+# Enlazar en test.elf
+riscv64-unknown-elf-gcc \
+    -march=rv32im \
+    -mabi=ilp32 \
+    -nostdlib \
+    -ffreestanding \
+    -g3 -gdwarf-4 \
+    startup.o main.o syscalls.o uart.o \
+    -T linker.ld \
+    -o test.elf
+
+if [ $? -eq 0 ]; then
+    echo "Compilación y enlace exitoso: test.elf generado"
+    echo "Archivos objeto: startup.o, main.o, syscalls.o, uart.o"
+else
+    echo "Error en el enlace"
+    exit 1
+fi
